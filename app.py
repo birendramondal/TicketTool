285e3c46de8e83254d62bf5be117461098cab87a
Birendra <birendramondal95@gmail.com>
2021-12-21 02:05:11 +0530

    link the Ajax call to load the graph and pass the python value to the graph.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,request
+import random
+from flask import Flask, render_template, request
 import mysql.connector  # Import MySql Connector
 from datetime import date, datetime
 
@@ -35,48 +36,57 @@ def Dashboard():
             mycursor = mydb.cursor()
             mycursor.execute("Select * from TicketTable")
             if True:
-                userdata=mycursor.fetchall()
-                return render_template('index.html',userdata=userdata)
+                userdata = mycursor.fetchall()
+                return render_template('index.html', userdata=userdata)
     except mysql.connector.Error as err:
-             print("Something went wrong: {}".format(err))
-             return render_template('index.html')
+        print("Something went wrong: {}".format(err))
+        return render_template('index.html')
 
 
+# Load the weekly graph
+
+@app.route("/weekdata", methods=["GET", "POST"])
+def weekdata():
+    weekdata = []
+    for i in range(1, 8):
+        weekdata.append(random.randint(7, 80))
+        data = {
+            "weekdata": weekdata
+        }
+    return data
 
 
-@app.route('/add', methods= ['GET','POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def addData():
     print("Test Pass")
     if (request.method == 'POST'):
-        name= request.form['agentName']
-        issue= request.form['issue']
-        priority= request.form['radio1']
-        status=request.form['Status Type']
-        bddate=datetime.today().strftime('%Y-%m-%d')
+        name = request.form['agentName']
+        issue = request.form['issue']
+        priority = request.form['radio1']
+        status = request.form['Status Type']
+        bddate = datetime.today().strftime('%Y-%m-%d')
 
-        print(name,issue,priority,status,bddate)
+        print(name, issue, priority, status, bddate)
         print("begin to execute and inside if")
 
-       
         try:
 
-          if(DBConnection() == True):
+            if(DBConnection() == True):
                 mycursor = mydb.cursor()
                 print("begin to execute")
                 # mycursor.execute(f"INSERT INTO TicketTable (TicketNo, priority, Status, AgentID, Issue, createdate) VALUES ('{3}','{priority}','{status}','{name}','{issue}','2021/05/07')")
-                mycursor.execute(f"INSERT INTO TicketTable (priority, Status, AgentID, Issue, createdate) VALUES ('{priority}','{status}','{name}','{issue}','{bddate}')")
+                mycursor.execute(
+                    f"INSERT INTO TicketTable (priority, Status, AgentID, Issue, createdate) VALUES ('{priority}','{status}','{name}','{issue}','{bddate}')")
                 # ALTER TABLE TicketTable CHANGE TicketNo TicketNo INT(50)AUTO_INCREMENT -----> Enable Auto increment in RDS
                 # mycursor.execute(f"INSERT INTO TicketTable (TicketNo, priority, Status, AgentID, Issue, createdate) VALUES ('5','{priority}','{status}','{name}','{issue}','{bddate}')")
                 mydb.commit()
                 print("Data comitted to DB")
                 return redirect("/")
         except mysql.connector.Error as err:
-             print("Something went wrong: {}".format(err))
+            print("Something went wrong: {}".format(err))
 
     else:
         print("not added")
-
-
 
 
 if __name__ == "__main__":
--- static/js/BarGraph.js
+++ static/js/BarGraph.js
@@ -1,32 +1,40 @@
 var xValues = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var yValues = [55, 49, 44, 34, 23, 38, 41];
-var barColors = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(201, 203, 207, 0.2)'
-];
+// var yValues = [55, 49, 44, 34, 23, 38, 41];
 
-new Chart("myBarChart", {
-    type: "bar",
-    data: {
-        labels: xValues,
-        datasets: [{
-            backgroundColor: barColors,
-            data: yValues
-        }]
-    },
-    options: {
-        legend: { display: false },
-        title: {
-            display: true,
-            text: "Weekly Ticket Data"
+var data = $.get('/weekdata');
+data.done(function (resp) {
+    console.log("JSON Data", resp.weekdata)
+
+    var barColors = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+    ];
+
+    new Chart("myBarChart", {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: resp.weekdata
+            }]
         },
-        scales: {
-            yAxes: [{ ticks: { min: 10, max: 70 } }],
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: "Weekly Ticket Data"
+            },
+            scales: {
+                yAxes: [{ ticks: { min: 10, max: 70 } }],
+            }
         }
-    }
-});
+    });
+})
+
+
